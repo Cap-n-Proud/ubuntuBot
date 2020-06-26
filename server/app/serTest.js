d9e1a885a35884526df70b96bccc42215923dc66
pfnegrini <kingsta@protonmail.com>
2020-06-26 10:34:25 +0100

    Commit from robot

--- server/app/serTest.js
+++ server/app/serTest.js
@@ -1,14 +1,22 @@
+// * @emits open
+// * @emits data
+// * @emits close
+// * @emits error
+// * @alias module:serialport
+
 const serPort = "/dev/ttyACM0"
 const serBaud = "9600"
 
 
 const com = require('serialport')
 const Readline = require('@serialport/parser-readline')
-const sPort = new SerialPort(serPort,{
-  baudrate: serBaud
+const sPort = new com(serPort,{
+  baudRate: Number(serPort)
 });
 
 const serialPort = sPort.pipe(new Readline({ delimiter: '\r\n' }))
+
+
 serialPort.on('open',function() {
   console.log('Arduino connected on '+ serPort + ' @' + serBaud);
 
