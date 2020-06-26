a741579d0a5023e7c044f63f275956417d4dec37
pfnegrini <kingsta@protonmail.com>
2020-06-26 23:47:52 +0200

    cleanup code

--- server/app/lib/functions.js
+++ server/app/lib/functions.js
@@ -1,19 +1,48 @@
-    function rescale(x, in_min, in_max, out_min, out_max) {
-        var output;
-        output = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-        //console.log(output);
-        if (output < out_min) {
-            output = out_min;
-        } else if (output > out_max) {
-            output = out_max;
-        } else {
-            //Nothing
-        }
-
-        return output
-
+function rescale(x, in_min, in_max, out_min, out_max) {
+    var output;
+    output = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    //console.log(output);
+    if (output < out_min) {
+        output = out_min;
+    } else if (output > out_max) {
+        output = out_max;
+    } else {
+        //Nothing
     }
-  
-   // exports ======================================================================
-  //exports.timeStamp = timeStamp;
-  exports.rescale = rescale;
+
+    return output
+
+}
+
+
+//Get IP address http://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js
+function findMyIP() {
+    var serverADDR = "N/A"
+    var os = require('os');
+    var ifaces = os.networkInterfaces();
+
+    Object.keys(ifaces).forEach(function(ifname) {
+        var alias = 0;
+
+        ifaces[ifname].forEach(function(iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+
+            if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                console.log(ifname + ':' + alias, iface.address);
+            } else {
+                // this interface has only one ipv4 adress
+                serverADDR = iface.address;
+            }
+        });
+    });
+    return serverADDR;
+}
+
+// exports ======================================================================
+//exports.timeStamp = timeStamp;
+exports.rescale = rescale;
+exports.findMyIP = findMyIP;
--- server/app/server.js
+++ server/app/server.js
@@ -1,10 +1,10 @@
 // sudo udevadm control --reload-rules
-
-
 var nconf = require('nconf');
 nconf.argv()
-       .env()
-       .file({ file: __dirname + '/config.json' });
+    .env()
+    .file({
+        file: __dirname + '/config.json'
+    });
 
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
@@ -16,17 +16,17 @@ var bunyan = require('bunyan');
 
 //--------------- Logging middleware ---------------
 var log = bunyan.createLogger({
-  name: 'bot',
-  streams: [
-    /*{
-      level: 'debug',
-      stream: process.stdout            // log INFO and above to stdout
-    },*/
-    //Log should be outside app folders
-    {
-      path: logfilePath + 'bot-Pilog.log'  // log ERROR and above to a file
-    }
-  ]
+    name: 'bot',
+    streams: [
+        /*{
+          level: 'debug',
+          stream: process.stdout            // log INFO and above to stdout
+        },*/
+        //Log should be outside app folders
+        {
+            path: logfilePath + 'bot-Pilog.log' // log ERROR and above to a file
+        }
+    ]
 });
 
 var fs = require('safefs');
@@ -39,15 +39,14 @@ var io = require('socket.io')(http);
 var sys = require('sys');
 var exec = require('child_process').exec;
 
-
 var serPort = nconf.get('server:serPort');
 var serBaud = nconf.get('server:serBaud');
 var serverPort = nconf.get('server:serverPort');
 var version = nconf.get('server:version');
 var videoFeedPort = nconf.get('video:port');
-var videoWidth = nconf.get('video:videoWidth');
-var videoHeight = nconf.get('video:videoHeight');
-var fps= nconf.get('video:fps');
+// var videoWidth = nconf.get('video:videoWidth');
+// var videoHeight = nconf.get('video:videoHeight');
+// var fps = nconf.get('video:fps');
 
 // include custom functions ======================================================================
 var systemModules = require(installPath + 'server/app/lib/systemModules');
@@ -66,91 +65,49 @@ app.use(express.static(installPath + 'server/wwwroot'));
 var serverADDR = 'N/A';
 var LogR = 0;
 var TelemetryFN = 'N/A';
-var prevTel="";
-var prevPitch="";
-var THReceived=0;
+var prevTel = "";
+var prevPitch = "";
+var THReceived = 0;
 
 var TelemetryHeader = 'N/A';
-var PIDHeader ='N/A';
+var PIDHeader = 'N/A';
 var ArduSysHeader;
-var Telemetry ={};
-var PID ={};
+var Telemetry = {};
+var PID = {};
 var PIDVal;
 var ArduSys = {};
 var temperature;
 
-/*
- Can use a try... catch statement
- var SerialPort = require("serialport").SerialPort
-
-try {
-  var serialPort = new SerialPort("/dev/portdoesntexist");
-} catch(error) {
-  console.log("Port not ready/doesn't exist!");
-}
-*/
-
-
-
+//Setup serial port
 const com = require('serialport')
 const Readline = require('@serialport/parser-readline')
-const sPort = new com(serPort,{
-  baudRate: Number(serPort)
+const sPort = new com(serPort, {
+    baudRate: Number(serPort)
 });
 
-const serialPort = sPort.pipe(new Readline({ delimiter: '\r\n' }))
+const serialPort = sPort.pipe(new Readline({
+    delimiter: '\r\n'
+}))
 
-
-sPort.on('open',function() {
-  console.log('Arduino connected on '+ serPort + ' @' + serBaud)
+sPort.on('open', function() {
+    console.log('Arduino connected on ' + serPort + ' @' + serBaud)
 })
 
 //serialPort.on('data', console.log)
 
 
 
-//Get IP address http://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js
-
-var os = require('os');
-var ifaces = os.networkInterfaces();
-
-Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0
-    ;
-
-  ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
-
-    if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
-      console.log(ifname + ':' + alias, iface.address);
-    } else {
-      // this interface has only one ipv4 adress
-      serverADDR = iface.address;
-    }
-  });
-});
-
-
-
-
 //---------------
 
 
-
-
-
-io.on('connection', function(socket){
-  //socket.emit('connected', version, Telemetry);
+io.on('connection', function(socket) {
+    //socket.emit('connected', version, Telemetry);
 
     var myDate = new Date();
 
-   var startMessage = 'Connected ' + myDate.getHours() + ':' + myDate.getMinutes() + ':' + myDate.getSeconds()+ ' v' + version + ' @' + serverADDR;
-  //Init the heades for telemtry data
-   serialPort.write('READ RemoteInit\n\r');
+    var startMessage = 'Connected ' + myDate.getHours() + ':' + myDate.getMinutes() + ':' + myDate.getSeconds() + ' v' + version + ' @' + serverADDR;
+    //Init the heades for telemtry data
+    serialPort.write('READ RemoteInit\n\r');
     //Trasmit system and PID parameters
 
     //socket.emit('serverADDR', serverADDR);
@@ -165,7 +122,7 @@ io.on('connection', function(socket){
     }, 2000);
 
 
-  /* Not needed as the info is displayed on screen
+    /* Not needed as the info is displayed on screen
    setInterval(function(){
   if(THReceived==1)socket.emit('status', Telemetry['yaw'], Telemetry['pitch'], Telemetry['roll'], Telemetry['bal'], Telemetry['dISTE']);
   if(Telemetry['pitch'] > 60)log.error('BALANCING FAIL! Pitch: ' + Telemetry['pitch']);
@@ -173,250 +130,246 @@ io.on('connection', function(socket){
   }, 250);
 */
 
-  setInterval(function(){
+    setInterval(function() {
 
-  var usage = "N/A";
-  temperature = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");
-  temperature = ((temperature/1000).toPrecision(3)) + "°C";
+        var usage = "N/A";
+        temperature = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");
+        temperature = ((temperature / 1000).toPrecision(3)) + "°C";
 
-  socket.emit("CPUInfo", temperature, usage);
-  }, 3 * 1000);
+        socket.emit("CPUInfo", temperature, usage);
+    }, 3 * 1000);
 
-  socket.on('Video', function(Video){
-   socket.emit('CMD', Video);
-    function puts(error, stdout, stderr) { sys.puts(stdout) }
-    exec('sudo bash ' + installPath + 'server/app/bin/' + Video, puts);
+    socket.on('Video', function(Video) {
+        socket.emit('CMD', Video);
+
+        function puts(error, stdout, stderr) {
+            sys.puts(stdout)
+        }
+        exec('sudo bash ' + installPath + 'server/app/bin/' + Video, puts);
 
     });
 
-  //Set commands goes to Arduino directly
-    socket.on('SCMD', function(CMD){
-    serialPort.write('SCMD ' + CMD + '\n');
-    log.debug('Command SCMD ' + CMD);
+    //Set commands goes to Arduino directly
+    socket.on('SCMD', function(CMD) {
+        serialPort.write('SCMD ' + CMD + '\n');
+        log.debug('Command SCMD ' + CMD);
     });
 
-    socket.on('move', function(dX, dY){
-		serialPort.write('SCMD move ' + Math.round(dX) + ' ' + Math.round(dY) + '\n');
+    socket.on('move', function(dX, dY) {
+        serialPort.write('SCMD move ' + Math.round(dX) + ' ' + Math.round(dY) + '\n');
 
-        });
+    });
 
-  //Server Commands
-  socket.on('SerCMD', function(CMD){
-    socket.emit('CMD', CMD);
-    if ( CMD == "LOG_ON" && !LogR) {
-      TelemetryFN = 'Telemetry_' + systemModules.timeStamp() + '.csv';
-      socket.emit('Info', telemetryfilePath+TelemetryFN)
-      log.debug('Telemetry logging started ' + telemetryfilePath + TelemetryFN);
+    //Server Commands
+    socket.on('SerCMD', function(CMD) {
+        socket.emit('CMD', CMD);
+        if (CMD == "LOG_ON" && !LogR) {
+            TelemetryFN = 'Telemetry_' + systemModules.timeStamp() + '.csv';
+            socket.emit('Info', telemetryfilePath + TelemetryFN)
+            log.debug('Telemetry logging started ' + telemetryfilePath + TelemetryFN);
 
-      systemModules.setTelemetryFile(telemetryfilePath, TelemetryFN, TelemetryHeader, PIDHeader, SEPARATOR);
-       LogR = 1;
+            systemModules.setTelemetryFile(telemetryfilePath, TelemetryFN, TelemetryHeader, PIDHeader, SEPARATOR);
+            LogR = 1;
 
-    }
-    else if ( CMD == "LOG_OFF" ){
-	socket.emit('Info', "logging stopped");
-	LogR = 0;
-	log.debug('Telemetry logging stopped ' + telemetryfilePath+TelemetryFN);
-    }
-    else if ( CMD == "showConfig" ){
-        fs.readFile(__dirname + '/config.json', 'utf8', function (err, json) {
-        if (err) throw err;
-	socket.emit('configSent', json);
-        });
-    }
-  });
+        } else if (CMD == "LOG_OFF") {
+            socket.emit('Info', "logging stopped");
+            LogR = 0;
+            log.debug('Telemetry logging stopped ' + telemetryfilePath + TelemetryFN);
+        } else if (CMD == "showConfig") {
+            fs.readFile(__dirname + '/config.json', 'utf8', function(err, json) {
+                if (err) throw err;
+                socket.emit('configSent', json);
+            });
+        }
+    });
 
-  socket.on('REBOOT', function(){
-    function puts(error, stdout, stderr) { sys.puts(stdout) }
-    log.info('Server rebootiing now');
-    exec('sudo reboot now');
-    sockets.emit('Info', "Rebooting")
+    socket.on('REBOOT', function() {
+        function puts(error, stdout, stderr) {
+            sys.puts(stdout)
+        }
+        log.info('Server rebootiing now');
+        exec('sudo reboot now');
+        sockets.emit('Info', "Rebooting")
 
-  });
+    });
 
-  socket.on('SHUTDOWN', function(){
-    socket.emit('Info', "Bailey going down for maintenance now!");
-    log.info('Bailey going down for maintenance now!');
-    function puts(error, stdout, stderr) { sys.puts(stdout) }
-    exec('sudo shutdown now');
+    socket.on('SHUTDOWN', function() {
+        socket.emit('Info', "Bailey going down for maintenance now!");
+        log.info('Bailey going down for maintenance now!');
 
-  });
+        function puts(error, stdout, stderr) {
+            sys.puts(stdout)
+        }
+        exec('sudo shutdown now');
 
-  socket.on('disconnect', function(){
-    console.log('Disconnected id: %s', socket.id);
-    log.info('Client disconnected ' + socket.id);
-  });
+    });
 
-  socket.on('connected', function(){
-    //console.log('CONNECTED id: %s', socket.id);
-   // log.info('Client disconnected ' + socket.id);sudo modprobe bcm2835-v4l2
-  });
+    socket.on('disconnect', function() {
+        console.log('Disconnected id: %s', socket.id);
+        log.info('Client disconnected ' + socket.id);
+    });
+
+    socket.on('connected', function() {
+        //console.log('CONNECTED id: %s', socket.id);
+        // log.info('Client disconnected ' + socket.id);sudo modprobe bcm2835-v4l2
+    });
 
 
-    eventEmitter.on('CMDecho', function(data){
+    eventEmitter.on('CMDecho', function(data) {
         socket.emit('CMD', data);
 
-  });
+    });
 
-    eventEmitter.on('serialData', function(data){
+    eventEmitter.on('serialData', function(data) {
         socket.emit('serialData', data);
 
-  });
+    });
 
 });
 
-io.on('disconnect', function () {
-        console.log('A socket with sessionID ' + hs.sessionID
-            + ' disconnected!');
-	log.info('A socket with sessionID ' + hs.sessionID
-            + ' disconnected!');
-    });
+io.on('disconnect', function() {
+    console.log('A socket with sessionID ' + hs.sessionID +
+        ' disconnected!');
+    log.info('A socket with sessionID ' + hs.sessionID +
+        ' disconnected!');
+});
 
-http.listen(serverPort, function(){
-console.log('listening on *: ' + serverADDR + ':' + serverPort + ' video feed: ' + videoFeedPort);
-log.info('Server listening on ' + serverADDR + ':' + serverPort + ' video feed: ' + videoFeedPort);
+http.listen(serverPort, function() {
+    console.log('listening on *: ' + serverADDR + ':' + serverPort + ' video feed: ' + videoFeedPort);
+    log.info('Server listening on ' + serverADDR + ':' + serverPort + ' video feed: ' + videoFeedPort);
 
 
-//Read input from Arduino and stores it into a dictionary
-serialPort.on('data', function(data, socket) {
-/*
-We store sensor data in arrays.
-0/ send command via serial to provide data
-1/ check what are we receiving (first letter of the trnasmission)
-2/ populate the correct variable
+    //Read input from Arduino and stores it into a dictionary
+    serialPort.on('data', function(data, socket) {
+        /*
+        We store sensor data in arrays.
+        0/ send command via serial to provide data
+        1/ check what are we receiving (first letter of the trnasmission)
+        2/ populate the correct variable
 
-*/
-	//"T" means we are receiving Telemetry data
+        */
+        //"T" means we are receiving Telemetry data
         //console.log(data);
         //this emits raw data from Arduino for debug purposes
         eventEmitter.emit('serialData', data);
         //console.log(data);
-        if (data.indexOf('SCMD') !== -1)
-	{
-          eventEmitter.emit('CMDecho', data);
+        if (data.indexOf('SCMD') !== -1) {
+            eventEmitter.emit('CMDecho', data);
         }
 
-        if (data.indexOf('T') !== -1)
-	{
-	  var tokenData = data.split(SEPARATOR);
-	  var j = 0;
+        if (data.indexOf('T') !== -1) {
+            var tokenData = data.split(SEPARATOR);
+            var j = 0;
 
-	  for (var i in Telemetry) {
-	    Telemetry[i] = tokenData[j];
-	    j++;
-	    //console.log(i + ' ' + Telemetry[i]);
-	  }
-	  j = 0;
+            for (var i in Telemetry) {
+                Telemetry[i] = tokenData[j];
+                j++;
+                //console.log(i + ' ' + Telemetry[i]);
+            }
+            j = 0;
 
-	  //eventEmitter.emit('log', data);
+            //eventEmitter.emit('log', data);
 
-	  if (LogR == 1){
-	    systemModules.addTelemetryRow(telemetryfilePath, TelemetryFN, TelemetryHeader, data, PIDHeader, PIDVal, SEPARATOR)
-	  }
-	}
+            if (LogR == 1) {
+                systemModules.addTelemetryRow(telemetryfilePath, TelemetryFN, TelemetryHeader, data, PIDHeader, PIDVal, SEPARATOR)
+            }
+        }
 
-	//"TH" means we are receiving Telemetry Headers
-        if (data.indexOf('TH') !== -1)
-	{
-          TelemetryHeader = data.split(SEPARATOR);
-	  var arrayLength = TelemetryHeader.length;
-	  for (var i = 0; i < arrayLength; i++) {
-	    Telemetry[TelemetryHeader[i]] = "N/A";
-	    //console.log(TelemetryHeader[i]);
-          }
+        //"TH" means we are receiving Telemetry Headers
+        if (data.indexOf('TH') !== -1) {
+            TelemetryHeader = data.split(SEPARATOR);
+            var arrayLength = TelemetryHeader.length;
+            for (var i = 0; i < arrayLength; i++) {
+                Telemetry[TelemetryHeader[i]] = "N/A";
+                //console.log(TelemetryHeader[i]);
+            }
 
-          THReceived=1;
-	  //eventEmitter.emit('log', data);
-	}
+            THReceived = 1;
+            //eventEmitter.emit('log', data);
+        }
 
-	if (data.indexOf('SYSH') !== -1)
-	{
-	  ArduSysHeader = data.split(SEPARATOR);
-	  var arrayLength = ArduSysHeader.length;
-	  for (var i = 0; i < arrayLength; i++) {
-	    ArduSys[ArduSysHeader[i]] = "N/A";
-	    //console.log(TelemetryHeader[i]);
-	}
-            setTimeout(function () {
-         	   serialPort.write('READ SYSParamTX\n\r');
+        if (data.indexOf('SYSH') !== -1) {
+            ArduSysHeader = data.split(SEPARATOR);
+            var arrayLength = ArduSysHeader.length;
+            for (var i = 0; i < arrayLength; i++) {
+                ArduSys[ArduSysHeader[i]] = "N/A";
+                //console.log(TelemetryHeader[i]);
+            }
+            setTimeout(function() {
+                serialPort.write('READ SYSParamTX\n\r');
 
             }, 100)
 
         }
 
-	if (data.indexOf('SYS') !== -1)
-	{
-	  var tokenData = data.split(SEPARATOR);
-	  var j = 0;
+        if (data.indexOf('SYS') !== -1) {
+            var tokenData = data.split(SEPARATOR);
+            var j = 0;
 
-	  for (var i in Telemetry) {
-	    ArduSys[i] = tokenData[j];
-	    j++;
-	    //console.log(i + ' ' + Telemetry[i]);
-	  }
-	  j = 0;
-	  //eventEmitter.emit('log', data);
-	}
+            for (var i in Telemetry) {
+                ArduSys[i] = tokenData[j];
+                j++;
+                //console.log(i + ' ' + Telemetry[i]);
+            }
+            j = 0;
+            //eventEmitter.emit('log', data);
+        }
 
-        if (data.indexOf('PID') !== -1)
-	{
-	  var tokenData = data.split(SEPARATOR);
-	  var j = 0;
-          PIDVal = "";
+        if (data.indexOf('PID') !== -1) {
+            var tokenData = data.split(SEPARATOR);
+            var j = 0;
+            PIDVal = "";
 
-	  for (var i in PID) {
-	    PID[i] = tokenData[j];
-            //PIDVal is used as a string to be concatenated in log file
-	    PIDVal = PIDVal + SEPARATOR + PID[i];
-	    j++;
-	     }
-	  j = 0;
-	  //log.info('PID values changed ' + PIDHeader + '\n' + PIDVal);
+            for (var i in PID) {
+                PID[i] = tokenData[j];
+                //PIDVal is used as a string to be concatenated in log file
+                PIDVal = PIDVal + SEPARATOR + PID[i];
+                j++;
+            }
+            j = 0;
+            //log.info('PID values changed ' + PIDHeader + '\n' + PIDVal);
 
-	}
+        }
 
-        if (data.indexOf('PIDH') !== -1)
-	{
-          PIDHeader = data.split(SEPARATOR);
-          var arrayLength = PIDHeader.length;
-	  for (var i = 0; i < arrayLength; i++) {
-	    PID[PIDHeader[i]] = "N/A";
-	    //console.log(PIDHeader[i]);// + ' ' + PID[PIDHeader[i]]);
-	  }
-	     setTimeout(function () {
+        if (data.indexOf('PIDH') !== -1) {
+            PIDHeader = data.split(SEPARATOR);
+            var arrayLength = PIDHeader.length;
+            for (var i = 0; i < arrayLength; i++) {
+                PID[PIDHeader[i]] = "N/A";
+                //console.log(PIDHeader[i]);// + ' ' + PID[PIDHeader[i]]);
+            }
+            setTimeout(function() {
                 serialPort.write('READ PIDParamTX\n\r');
             }, 100);
         }
 
-	//Change the first word to be 'ArduConfig'
-	//If the first word is '***' prints in the server console. Used to debug the config from Arduino
-	if (data.indexOf('***') !== -1)
-	{
-	  console.log(data);
-	  log.info('Configuration received from Arduino: ' + data);
+        //Change the first word to be 'ArduConfig'
+        //If the first word is '***' prints in the server console. Used to debug the config from Arduino
+        if (data.indexOf('***') !== -1) {
+            console.log(data);
+            log.info('Configuration received from Arduino: ' + data);
 
-	}
+        }
 
-	//Handle errors from Arduino
-	if (data.indexOf('E') !== -1)
-	{
-          log.error('ERROR: ' + data);
+        //Handle errors from Arduino
+        if (data.indexOf('E') !== -1) {
+            log.error('ERROR: ' + data);
         }
 
 
-	//IS THIS SILL RELEVANT?
-	//Get the header for the object that stores telemetry data
-	if (data.indexOf('HEADER') !== -1)
-	{
-	  TelemetryHeader = data.split(SEPARATOR);
-	  var arrayLength = TelemetryHeader.length;
-	  for (var i = 0; i < arrayLength; i++) {
-	    Telemetry[TelemetryHeader[i]] = "N/A";
-	    //console.log(TelemetryHeader[i]);
-	  }
-	}
+        //IS THIS SILL RELEVANT?
+        //Get the header for the object that stores telemetry data
+        if (data.indexOf('HEADER') !== -1) {
+            TelemetryHeader = data.split(SEPARATOR);
+            var arrayLength = TelemetryHeader.length;
+            for (var i = 0; i < arrayLength; i++) {
+                Telemetry[TelemetryHeader[i]] = "N/A";
+                //console.log(TelemetryHeader[i]);
+            }
+        }
 
 
-});
+    });
 
 });
 
