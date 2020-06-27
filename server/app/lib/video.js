ea172d3d578aba79f34b73cdedca7d110cee96be
pfnegrini <kingsta@protonmail.com>
2020-06-27 08:28:51 +0200

    e

--- server/app/lib/video.js
+++ server/app/lib/video.js
@@ -105,7 +105,7 @@ function drawCompass(im, videoWidth, videoHeight, heading) {
         if (i % 20 == 0)
         //im.drawLine(new cv.Point(reduce(i + heading, compassRange) + minI, -screenMargin), new cv.Point(reduce(i + heading, compassRange) + minI, screenMargin + 10), new cv.Vec(Number(hudColor)));
         console.log(Number('HUD: ' + hudColor));
-         im.drawLine(new cv.Point(10, 10), new cv.Point(10,  10), new cv.Vec(Number(hudColor)));
+         im.drawLine(new cv.Point(10, 10), new cv.Point(10,  10), new cv.Vec(Number(screenMargin,screenMargin,screenMargin)));
         if (i % 10 == 0)
             im.drawLine(new cv.Point(reduce(i + heading, compassRange) + minI, -screenMargin), new cv.Point(reduce(i + heading, compassRange) + minI, screenMargin + 5), new cv.Vec(Number(hudColor)));
         if (i == map(0, 0, 360, minI, maxI))
