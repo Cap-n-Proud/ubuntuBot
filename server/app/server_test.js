c3f1ae172ffdb6c6991bcc9c0f37d39ce510625e
pfnegrini <kingsta@protonmail.com>
2020-06-29 11:26:10 +0200

    t

--- server/app/server_test.js
+++ server/app/server_test.js
@@ -1,3 +1,11 @@
+var nconf = require('nconf');
+nconf.argv()
+    .env()
+    .file({
+        file: __dirname + '/config.json'
+    });
+
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -10,9 +18,14 @@ require('./routes')(app);
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    console.log(msg);
   });
 });
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+
+
+module.exports.nconf = nconf;
