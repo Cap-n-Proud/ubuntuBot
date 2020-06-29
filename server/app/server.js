d6f99cb34b3cb6cee7d868968784948b210366a8
pfnegrini <kingsta@protonmail.com>
2020-06-29 10:51:06 +0200

    test

--- server/app/server.js
+++ server/app/server.js
@@ -222,7 +222,25 @@ io.on('connection', function(socket) {
 
 io.on('disconnect', function() {
     console.log('A socket with sessionID ' + hs.sessionID +
-        ' disconnected!');
+      var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+  ' disconnected!');
     log.info('A socket with sessionID ' + hs.sessionID +
         ' disconnected!');
 });
--- server/app/server_test.js
+++ server/app/server_test.js
@@ -1,57 +1,18 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+//var port = process.env.PORT || 3000;
+var port =3000;
 
-// include custom functions ======================================================================
-//var systemModules = require(installPath + 'server/app/lib/systemModules');
-var functions = require(__dirname + '/lib/functions');
-const serverPort = 54321
-const robotName ="testserver"
-const videoFeedPort ="N/A"
-const version ="0.1"
-var serverADDR = functions.findMyIP();
+// load the routes
+require('./routes')(app);
 
-
-io.on('connection', function(socket) {
-    //socket.emit('connected', version, Telemetry);
-
-    var myDate = new Date();
-    var startMessage = 'Connected ' + myDate.getHours() + ':' + myDate.getMinutes() + ':' + myDate.getSeconds() + ' v' + version + ' @' + serverADDR;
-    console.log('New socket.io connection - id: %s', socket.id);
-
-
-    socket.on('disconnect', function() {
-        console.log('Disconnected id: %s', socket.id);
-    });
-
-    socket.on('connected', function() {
-        console.log('CONNECTED id: %s', socket.id);
-        // log.info('Client disconnected ' + socket.id);sudo modprobe bcm2835-v4l2
-    });
-
-
-
-
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
-io.on('disconnect', function() {
-    console.log('A socket with sessionID ' + hs.sessionID +
-        ' disconnected!');
-
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
-
-http.listen(serverPort, function() {
-    console.log(robotName + ' listening on: ' + serverADDR + ':' + serverPort + ' video feed: ' + videoFeedPort);
-    //log.info(robotName + ' listening on ' + serverADDR + ':' + serverPort + ' video feed: ' + videoFeedPort);
-
-
-    //Read input from Arduino and stores it into a dictionary
-
-
-});
-
-//
-// module.exports.Telemetry = Telemetry;
-// module.exports.temperature = temperature;
-// module.exports.nconf = nconf;
