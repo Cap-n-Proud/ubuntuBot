40cf32659c5072f32530af144d05503499e81a98
pfnegrini <kingsta@protonmail.com>
2020-06-29 10:32:02 +0200

    se

--- server/app/server_test.js
+++ server/app/server_test.js
@@ -1,108 +1,14 @@
-// sudo udevadm control --reload-rules
-var nconf = require('nconf');
-nconf.argv()
-    .env()
-    .file({
-        file: __dirname + '/config.json'
-    });
-
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-var nodeLib = nconf.get('server:nodeLib');
-var logfilePath = nconf.get('server:logfilePath');
-
-var telemetryfilePath = nconf.get('telemetry:telemetryfilePath');
-var bunyan = require('bunyan');
-
-//--------------- Logging middleware ---------------
-var log = bunyan.createLogger({
-    name: 'bot',
-    streams: [
-        /*{
-          level: 'debug',
-          stream: process.stdout            // log INFO and above to stdout
-        },*/
-        //Log should be outside app folders
-        {
-            path: logfilePath + 'bot-Pilog.log' // log ERROR and above to a file
-        }
-    ]
-});
-
-var fs = require('safefs');
-var SEPARATOR = nconf.get('telemetry:SEPARATOR');
-var installPath = nconf.get('server:installPath');
-const robotName = nconf.get('server:name');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var sys = require('sys');
-var exec = require('child_process').exec;
-
-var serPort = nconf.get('server:serPort');
-var serBaud = nconf.get('server:serBaud');
-var serverPort = nconf.get('server:serverPort');
-var version = nconf.get('server:version');
-var videoFeedPort = nconf.get('video:port');
-var videoWidth = nconf.get('video:videoWidth');
-var videoHeight = nconf.get('video:videoHeight');
-var fps = nconf.get('video:fps');
-
 
 // include custom functions ======================================================================
 //var systemModules = require(installPath + 'server/app/lib/systemModules');
 var functions = require(installPath + 'server/app/lib/functions');
-var camera = require(installPath + 'server/app/lib/camera');
-var videoFeed = require(installPath + 'server/app/lib/video');
 
-
-//var robot = require(installPath + 'server/app/robot');
-
-// load the routes
-require('./routes')(app);
-
-app.use(express.static(installPath + 'server/wwwroot'));
-
-//var serverADDR = 'N/A';
 var serverADDR = functions.findMyIP();
 
-var LogR = 0;
-var TelemetryFN = 'N/A';
-var prevTel = "";
-var prevPitch = "";
-var THReceived = 0;
-
-var TelemetryHeader = 'N/A';
-var PIDHeader = 'N/A';
-var ArduSysHeader;
-var Telemetry = {};
-var PID = {};
-var PIDVal;
-var ArduSys = {};
-var temperature;
-
-//Setup serial port
-const com = require('serialport')
-const Readline = require('@serialport/parser-readline')
-const sPort = new com(serPort, {
-    baudRate: Number(serPort)
-});
-
-const serialPort = sPort.pipe(new Readline({
-    delimiter: '\r\n'
-}))
-
-sPort.on('open', function() {
-    console.log('Arduino connected on ' + serPort + ' @' + serBaud)
-})
-
-//serialPort.on('data', console.log)
-// var os = require( 'os' );
-//
-// var networkInterfaces = os.networkInterfaces( );
-//
-// console.log( networkInterfaces );
 
 io.on('connection', function(socket) {
     //socket.emit('connected', version, Telemetry);
@@ -114,7 +20,6 @@ io.on('connection', function(socket) {
 
     socket.on('disconnect', function() {
         console.log('Disconnected id: %s', socket.id);
-        log.info('Client disconnected ' + socket.id);
     });
 
     socket.on('connected', function() {
@@ -130,8 +35,7 @@ io.on('connection', function(socket) {
 io.on('disconnect', function() {
     console.log('A socket with sessionID ' + hs.sessionID +
         ' disconnected!');
-    log.info('A socket with sessionID ' + hs.sessionID +
-        ' disconnected!');
+
 });
 
 http.listen(serverPort, function() {
